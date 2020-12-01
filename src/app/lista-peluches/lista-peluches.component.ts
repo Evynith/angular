aa51565b1b67952fd0c4348a72d75f1b6ccf2247
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2020-12-01 11:11:16 -0300

    creacion componente peluhe

--- src/app/lista-peluches/lista-peluches.component.ts
+++ src/app/lista-peluches/lista-peluches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { peluches } from './peluches';
 
 @Component({
   selector: 'app-lista-peluches',
@@ -7,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPeluchesComponent implements OnInit {
 
-  peluche1 = {
-    "nombre" : "Pulpito",
-    "material" : "felpa",
-    "precio" : 123,
-    "stock" : 10,
-    "imagen" : "assets/images/pulpito.jpg"
-  }
+  peluches : peluches[] = 
+  [
+    {
+      nombre : "Pulpito",
+      material : "felpa",
+      precio : 123,
+      stock : 10,
+      imagen : "assets/images/pulpito.jpg"
+    },
+    {
+      nombre : "Stitch",
+      material : "felpa",
+      precio : 456,
+      stock : 11,
+      imagen : "assets/images/stitch.webp"
+    },
+    {
+      nombre : "Yoda",
+      material : "felpa",
+      precio : 789,
+      stock : 12,
+      imagen : "assets/images/yoda.jpeg"
+    }
+  ];
+  
 
   constructor() { }
 
---
+++ src/app/lista-peluches/peluches.ts
@@ -0,0 +1,7 @@
+export interface peluches{
+    nombre : string;
+    material : string;
+    precio : number;
+    stock : number;
+    imagen : string;
+}
